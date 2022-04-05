6dab62b7d228d27f8cd6de59be5637c3cd784354
Tian Yao <yao.tian@avient.com>
2022-04-05 15:46:52 +0800

    add session

--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module myGoApp
 go 1.15
 
 require (
+	github.com/alexedwards/scs/v2 v2.5.0
 	github.com/go-chi/chi/v5 v5.0.7
 	github.com/justinas/nosurf v1.1.1
 )
--- main.go
+++ main.go
@@ -8,12 +8,27 @@ import (
 	handerls "myGoApp/pkg/handlers"
 	"myGoApp/pkg/render"
 	"net/http"
+	"time"
+
+	"github.com/alexedwards/scs/v2"
 )
 
 const portNumber = ":8080"
 
+var app config.AppConfig
+var session *scs.SessionManager
+
 func main() {
-	var app config.AppConfig
+
+	app.InProduction = false
+
+	session = scs.New()
+	session.Lifetime = 24 * time.Hour
+	session.Cookie.Persist = true
+	session.Cookie.SameSite = http.SameSiteLaxMode
+	session.Cookie.Secure = app.InProduction
+
+	app.Session = session
 
 	tc, err := render.CreateTemplateCache()
 	if err != nil {
